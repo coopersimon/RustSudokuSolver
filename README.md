15f6f6e50daa33471502aebae4a127af7475043e
coopersimon <simon.d.cooper@hotmail.co.uk>
2016-11-29 00:28:38 +0000

    initial commit. got it working

---
+++ README.md
@@ -0,0 +1,5 @@
+## Rust Sudoku Solver
+Simple sudoku solver written in rust. My first proper rust project, so it's a little ugly.
+The algorithm works but I think the code is fairly unsafe. I may come back to this later (work permitting).
+
+Sudoku can be entered manually, or by putting the name of a file as the first argument. File just needs the 81 numbers entered in sequence. Use "0" to represent an unknown value.
---
+++ src/main.rs
@@ -0,0 +1,220 @@
+use std::env;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main()
+{
+      let mut sudoku = Vec::new();
+      // load from file if necessary
+      if let Some(file_name) = env::args().nth(1)
+      {
+            let file_contents = match read_file(file_name)
+            {
+                  Ok(s) => s,
+                  Err(err) => 
+                  {
+                        println!("couldn't read file: {}", err);
+                        return;
+                  }
+            };
+            // copy sudoku into vector
+            sudoku = file_contents.split_whitespace()
+                        .map(|n| n.parse().unwrap())
+                        .collect();
+
+            if sudoku.len() != 81
+            {
+                  println!("sudoku isn't correct size");
+                  return;
+            }
+      }
+      else
+      {
+            println!("Input sudoku. Use \'0\' to represent blanks.");
+            // input sudoku
+            for _ in 0..81
+            {
+                  let mut num = String::new();
+                  io::stdin().read_line(&mut num)
+                        .expect("failed to read");
+
+                  let num: u32 = num.trim().parse()
+                        .expect("not a number");
+
+                  sudoku.push(num);
+            }
+      }
+
+      let mut solution = sudoku.clone();
+      // fill first row, recursively fill other rows
+      let blank = Vec::new();
+      if !fill_sudoku(&sudoku, 0, 0, &mut solution, &blank)
+      {
+            println!("invalid sudoku");
+      }
+      else
+      {
+            print_sudoku(&solution);
+      }
+      // if returns true, print sudoku
+      // if false, invalid
+
+}
+
+fn fill_sudoku(init: &Vec<u32>, row_num: usize, col_num: usize, mut solution: &mut Vec<u32>, row_blanks: &Vec<u32>) -> bool
+{
+      // check if we're done.
+      if row_num >= 9
+      {
+            return true;
+      }
+
+      // check if we're at the end of a row
+      if col_num >= 9
+      {
+            return fill_sudoku(&init, row_num + 1, 0, &mut solution, &row_blanks);
+      }
+
+      // value of current square
+      let offset = (row_num * 9) + col_num;
+
+      // possible values for the row's remaining blanks
+      let input_blanks;
+
+      // if at the start of a row, need to calculate missing values
+      if col_num == 0
+      {
+            // work out missing numbers in row and fill vector
+            let mut row_blanks = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+            //let mut solve_row = start
+            let begin = row_num * 9;
+            let end = 9 + (row_num * 9);
+            for x in begin..end
+            {
+                  // remove number
+                  row_blanks.retain(|&y| y != init[x]);
+            }
+            // clear solution
+            for x in begin..end
+            {
+                  solution[x] = init[x];
+            }
+
+            input_blanks = row_blanks.clone();
+      }
+      // otherwise just copy in from input
+      else
+      {
+            input_blanks = row_blanks.clone();
+      }
+
+      // possible values for the square's remaining blanks
+      let mut blanks = input_blanks.clone();
+
+      // if blank is already filled...
+      if solution[offset] != 0
+      {
+            // simply check next square.
+            return fill_sudoku(&init, row_num, col_num + 1, &mut solution, &input_blanks);
+      }
+
+      // loop until return.
+      loop
+      {
+            // if all options have been exhausted, finish looping
+            if blanks.is_empty()
+            {
+                  solution[offset] = 0;
+                  return false;
+            }
+
+            solution[offset] = blanks.pop().unwrap();
+
+            // check if the value is invalid according to the column and box
+            if !check_column(&solution, col_num) || !check_box(&solution, (row_num / 3) * 3, (col_num / 3) * 3)
+            {
+                  continue;
+            }
+
+            // work out blanks for next square
+            let mut next_blanks = input_blanks.clone();
+            next_blanks.retain(|&x| x != solution[offset]);
+
+            // rest of row is valid, return true
+            if fill_sudoku(&init, row_num, col_num + 1, &mut solution, &next_blanks)
+            {
+                  return true;
+            }
+      }
+}
+
+// check the column is valid
+fn check_column(sudoku: &Vec<u32>, col_num: usize) -> bool
+{
+      let mut found: Vec<u32> = Vec::new();
+      for y in 0..9
+      {
+            let index = col_num + y*9;
+            if found.contains(&sudoku[index])
+            {
+                  return false;
+            }
+            else if sudoku[index] != 0
+            {
+                  found.push(sudoku[index]);
+            }
+      }
+      true
+}
+
+// check the box is valid
+fn check_box(sudoku: &Vec<u32>, row_num: usize, col_num: usize) -> bool
+{
+      let mut found: Vec<u32> = Vec::new();
+      let offset = row_num * 9;
+      for x in 0..3
+      {
+            for y in 0..3
+            {
+                  let index = offset + col_num + x + y*9;
+                  if found.contains(&sudoku[index])
+                  {
+                        return false;
+                  }
+                  else if sudoku[index] != 0
+                  {
+                        found.push(sudoku[index]);
+                  }
+            }
+      }
+      true
+}
+
+fn read_file(file_name: String) -> Result<String, String>
+{
+      let mut input_file = try!(File::open(file_name).map_err(|e| e.to_string()));
+      let mut file_contents = String::new();
+      try!(input_file.read_to_string(&mut file_contents).map_err(|e| e.to_string()));
+      Ok(file_contents)
+}
+
+fn print_sudoku(sudoku: &Vec<u32>) -> bool
+{
+      if sudoku.len() != 81
+      {
+            return false;
+      }
+
+      for y in 0..9
+      {
+            let offset = y * 9;
+            for x in 0..9
+            {
+                  print!("{} ", sudoku[offset + x]);
+            }
+            print!("\n");
+      }
+
+      true
+}
